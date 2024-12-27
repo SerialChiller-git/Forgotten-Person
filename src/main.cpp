19306dd7fbb17aec5dc011e84cfdb212b0cf5441
Serial CHiller <67603638+Wise-Dude@users.noreply.github.com>
2024-12-28 04:57:06 +0600

    changed the map

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,25 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({800, 600}), "Forgotten Person");
+    auto window = sf::RenderWindow(sf::VideoMode({512, 256}), "Forgotten Person");
     window.setFramerateLimit(75);
     
     constexpr std::array level = {
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    0, 1, 2, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4,
+    0, 1, 0, 0, 3, 0, 4, 4, 4, 0, 1, 1, 1, 0, 0, 0,
+    0, 1, 1, 0, 4, 4, 4, 0, 0, 0, 1, 1, 1, 3, 0, 0,
+    0, 0, 1, 0, 4, 0, 3, 3, 0, 0, 1, 1, 1, 1, 3, 0,
+    3, 0, 1, 0, 4, 0, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1,
+    0, 0, 1, 0, 4, 3, 3, 3, 0, 0, 0, 0, 1, 10, 11, 1,
     };
 
     gameTile map;
-    if (!map.load("assets/tileset-grassland-grass.png", {32, 32}, level.data(), 16, 8))
+    if (!map.load("assets/levelSet.png", {32, 32}, level.data(), 16, 8))
         return -1;
 
-
+    
     while (window.isOpen())
     {   
         while (const std::optional event = window.pollEvent())
