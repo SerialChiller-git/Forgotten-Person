592c44303fe25fb67d8f63edb399b7ca735d8ac6
Serial CHiller <67603638+Wise-Dude@users.noreply.github.com>
2024-12-29 02:27:44 +0600

    added a sprite loading system

--- src/MapEditor/editor.cpp
+++ src/MapEditor/editor.cpp
@@ -6,15 +6,18 @@
 #include "guiManager.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({512, 512}), "ImGui + SFML = <3");
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "TileMap Editor");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
-    //======== codes for background colour ==============
+    //======== variables to modify the Gui ==============
 
     float color[3] = {0.0f, 0.0f, 0.0f};
     sf::Color bgcolor;
-
+    int inputValue = 32;
+    sf::Texture defaultTexture(sf::Vector2u{200,200});
+    sf::Sprite sprite(defaultTexture);
+    bool isLoaded = false;
     //============== objects ==========================
 
     guiManager gui;
@@ -34,7 +37,7 @@ int main() {
         ImGui::SFML::Update(window, deltaClock.restart());
 
        
-
+        // Start of Gui , will move it to a method later
         ImGui::Begin("Controls");
 
         if(ImGui::ColorEdit3("Backgorund color", color)){
@@ -42,12 +45,24 @@ int main() {
             bgcolor.g = static_cast<std::uint8_t>(color[1] * 255.0f);
             bgcolor.b = static_cast<std::uint8_t>(color[2] * 255.0f);
         }
-        ImGui::SliderInt("grid X", &gui.x, 1, 100);
-        ImGui::SliderInt("grid Y", &gui.y, 1, 100);
-
+        ImGui::SliderInt("grid X", &gui.x, 1, 128);
+        ImGui::SliderInt("grid Y", &gui.y, 1, 128);
+        if(ImGui::InputInt("Square val (x,y)" , &inputValue)){
+            gui.x = inputValue;
+            gui.y = inputValue;
+        }
         ImGui::End();
+        
+        gui.spriteWindow(window, sprite, isLoaded, defaultTexture);
+        window.clear(bgcolor);   // Set everything that I want to see below this except the ImGui
+        
+        if(isLoaded){
+            sprite.setPosition({10.0f, 10.0f});
+            std::cout << "LOADED";
+            window.draw(sprite);
+        }
 
-        window.clear(bgcolor);
+        
         gui.displayGrids(window);
         ImGui::SFML::Render(window);
         window.display();
--- src/MapEditor/guiManager.h
+++ src/MapEditor/guiManager.h
@@ -2,7 +2,7 @@
 #include <imgui.h>
 #include <imgui-SFML.h>
 #include<SFML/Graphics.hpp>
-
+#include<iostream>
 
 class guiManager{
     public:
@@ -12,10 +12,25 @@ class guiManager{
         unsigned int win_x = 512;
         unsigned int win_y = 512;
 
-        void spriteWindow(){
+        char path[256];
+        guiManager(){
+            memset(path, 0, 256);
+        }
+
+        void spriteWindow(sf::RenderWindow &window, sf::Sprite &sprite, bool &isLoaded, sf::Texture &texture){
             ImGui::Begin("Sprites");
-
-
+                ImGui::InputText("Path to Sprite" , path ,256);
+                
+                if(ImGui::Button("Load")){
+                    if(texture.loadFromFile(path)){
+                        sprite.setTexture(texture);
+                        isLoaded = true;
+                    }
+                    else{
+                        std::cerr << "Texture not loaded properly \n";
+                        isLoaded = false;
+                    }
+                }
             ImGui::End();
         }
 
@@ -29,11 +44,17 @@ class guiManager{
             sf::VertexArray lines(sf::PrimitiveType::Lines);
 
             for(int i = 0; i < win_x; i++){
+                if(i*x > win_x){
+                    break;
+                }
                 lines.append({sf::Vector2f(i*x, 0), sf::Color::White});
                 lines.append({sf::Vector2f(i*x, win_y), sf::Color::White});
             }
 
             for(int i = 0; i < win_y; i++){
+                if(i*y > win_y){
+                    break;
+                }
                 lines.append({sf::Vector2f(0, i*y), sf::Color::White});
                 lines.append({sf::Vector2f(win_x, i*y), sf::Color::White});
             }
