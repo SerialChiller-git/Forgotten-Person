0907ee740bb1dc7d38d2254aa381534188f445f0
Serial CHiller <67603638+Wise-Dude@users.noreply.github.com>
2025-01-01 03:20:56 +0600

    added player

---
+++ src/Player.h
@@ -0,0 +1,49 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<filesystem>
+
+class Player : public sf::Drawable, public sf::Transformable{
+    public:
+        
+        int health;
+        sf::Texture texture;
+        int spriteWidth;
+        int spriteHeight;
+        sf::Sprite sprite;
+        int speed;
+        Player(): sprite(texture){
+            speed = 200;
+            health = 100;
+            sprite.setPosition({0, 0});
+            sprite.setScale({1, 1});
+            
+        }
+
+        bool loadTexture(const std::filesystem::path& path){
+            if(!texture.loadFromFile(path)){
+                return false;
+            }
+            sprite.setTexture(texture);
+            return true;
+        }
+
+        void setFrame(int i, int spriteWidth = 64, int spriteHeight = 64){
+            this->spriteWidth = spriteWidth;
+            this->spriteHeight = spriteHeight;
+            sprite.setTextureRect(sf::IntRect({i*spriteWidth, 0} , {spriteWidth, spriteHeight}));
+        }
+
+        void takeDamage(int damage){
+            health -= damage;
+        }
+
+        void heal(int heal){
+            health += heal;
+        }
+    private:
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const override{
+            states.transform *= getTransform();
+            target.draw(sprite, states);
+        }
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
 #include "gameTile.h"
+#include "Player.h"
+
+sf::Texture playerTexture;
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({512, 256}), "Forgotten Person");
+    auto window = sf::RenderWindow(sf::VideoMode({1024, 512}), "Forgotten Person");
     window.setFramerateLimit(75);
     
-    constexpr std::array level = {
+  constexpr std::array level = {
     0, 1, 2, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0,
     1, 1, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4,
@@ -17,15 +20,40 @@ int main()
     0, 0, 1, 0, 4, 0, 3, 3, 0, 0, 1, 1, 1, 1, 3, 0,
     3, 0, 1, 0, 4, 0, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1,
     0, 0, 1, 0, 4, 3, 3, 3, 0, 0, 0, 0, 1, 10, 11, 1,
+    0, 1, 2, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4,
+    0, 1, 0, 0, 3, 0, 4, 4, 4, 0, 1, 1, 1, 0, 0, 0,
+    0, 1, 1, 0, 4, 4, 4, 0, 0, 0, 1, 1, 1, 3, 0, 0,
+    0, 0, 1, 0, 4, 0, 3, 3, 0, 0, 1, 1, 1, 1, 3, 0,
+    3, 0, 1, 0, 4, 0, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1,
+    0, 0, 1, 0, 4, 3, 3, 3, 0, 0, 0, 0, 1, 10, 11, 1
     };
 
+
     gameTile map;
-    if (!map.load("assets/levelSet.png", {32, 32}, level.data(), 16, 8))
+    if (!map.load("assets/levelSet.png", {32, 32}, level.data(), 16, 16))
         return -1;
 
+    Player player;
+    if(!player.loadTexture("assets/player.png")){
+        std::cerr << "Texture not loaded properly \n";
+    }
+
+    map.setScale({2, 1});
+
+    player.setFrame(1, 64, 64);
     
+    
+
+    
+    sf::Clock deltaClock;
+    sf::Time deltaTime;
     while (window.isOpen())
     {   
+        deltaTime = deltaClock.restart();
+        float moveDistance = player.speed * deltaTime.asSeconds();
+        sf::Vector2f movement = {0.0f, 0.0f};
         while (const std::optional event = window.pollEvent())
         {
             if(event ->is<sf::Event::Closed>()){
@@ -37,17 +65,39 @@ int main()
                 case sf::Keyboard::Scancode::Escape:
                     window.close();
                     break;
-                case sf::Keyboard::Scancode::A:
-                    break;
+                
                 default:
                     break;
                 }
             }
-            
         }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
+                    player.setFrame(3, 64, 64);
+                    movement.x -= moveDistance;
+                    
+                    }
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
+                    player.setFrame(2, 64, 64);
+                    movement.x += moveDistance;
+                    
+                    }
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
+                    player.setFrame(0, 64, 64);
+                    movement.y -= moveDistance;
+                    
+                    }
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
+                    player.setFrame(1, 64, 64);
+                    movement.y += moveDistance;
+                    
+                 }
+        std::cout << movement.x << " " << movement.y << "\n";
+        player.move(movement);
+        
 
         window.clear(sf::Color::Black);
         window.draw(map);
+        window.draw(player);
         window.display();
     }
 }
